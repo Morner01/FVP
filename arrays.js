da8963cc10be8533bd2ad700b64f0d78c101b333
Morner <van.maltsev2004@gmail.com>
2024-02-16 10:26:21 +0300

    v1

---
+++ arrays.js
@@ -0,0 +1,25 @@
+const names = ['Иван', 'Мария', 'Алексей', 'Ольга', 'Сергей', 'Анна']; // 1
+
+names.forEach((elem) => console.log(`Привет, ${elem}`));
+
+const array = [1, 2, 3, 4, 5];
+
+const arr = (array.map((x) => x * 10));
+console.log(arr);
+
+const array1 = [5, 12, 8, 130, 44];
+
+const arr1 = (array1.filter((x) => x > 10));
+console.log(arr1)
+
+const mas = [
+  { name: 'Иван', age: 23 }, 
+  { name: 'Мария', age: 18 }, 
+  { name: 'Алексей', age: 32 }, 
+  { name: 'Ольга', age: 24 }, 
+  { name: 'Сергей', age: 17 }, 
+  { name: 'Анна', age: 21 },
+];
+const ages = mas.filter(person => person.age > 18);
+const filt = ages.map(person => `${person.name} (${person.age} лет)`).join(", ");
+console.log(filt);
